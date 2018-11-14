25c45373da0c7ce9eafae6067b22b24bceb6b6eb
aleksanderwozniak <aleksanderwozniak96@gmail.com>
2018-11-14 10:47:36 +0100

    Improve AddedOn and DueOn Date formatting

--- lib/presentation/shared/helper/date_formatter.dart
+++ lib/presentation/shared/helper/date_formatter.dart
@@ -17,17 +17,6 @@ class DateFormatter {
     return formatFull(date);
   }
 
-  static String formatDays(DateTime date) {
-    final today = DateTime.now();
-    final resetToday = DateTime.utc(today.year, today.month, today.day);
-
-    // 'resets' are for ensuring proper difference calculation
-    final resetDate = DateTime.utc(date.year, date.month, date.day);
-    final difference = resetDate.difference(resetToday);
-
-    return _formatDaysDifference(difference.inDays);
-  }
-
   static String safeFormatDays(DateTime date) {
     if (date == null) {
       return '';
@@ -36,23 +25,70 @@ class DateFormatter {
     return formatDays(date);
   }
 
-  static String _formatDaysDifference(int inDays) {
-    // TODO: [WIP]
-    if (inDays == 0) {
+  static String formatDays(DateTime date) {
+    final today = DateTime.now();
+    final resetToday = DateTime.utc(today.year, today.month, today.day);
+
+    // 'resets' are for ensuring proper difference calculation
+    final resetDate = DateTime.utc(date.year, date.month, date.day);
+    final difference = resetDate.difference(resetToday);
+    final days = difference.inDays;
+
+    if (days == 0) {
       return 'Today';
-    } else if (inDays > 0) {
-      if (inDays == 1) {
-        return 'Tomorrow';
-      } else {
-        return '$inDays days';
-      }
-    } else {
-      final daysAbs = inDays.abs();
-      if (inDays == -1) {
+    }
+
+    if (difference.isNegative) {
+      // past
+      final daysAbs = days.abs();
+      if (daysAbs == 1) {
         return 'Yesterday';
-      } else {
-        return '$daysAbs days ago';
       }
+
+      if (daysAbs < 7) {
+        return DateFormat("'Last' EEEE ($days 'days ago')").format(date);
+      }
+
+      if (daysAbs < 30) {
+        final weeks = (daysAbs / 7).truncate();
+        return _pastPlural(text: 'week', value: weeks);
+      }
+
+      final months = (daysAbs / 30).truncate();
+      return _pastPlural(text: 'month', value: months);
+    } else {
+      // future
+      if (days == 1) {
+        return 'Tomorrow';
+      }
+
+      if (days < 7) {
+        return DateFormat("'Next' EEEE ($days 'days')").format(date);
+      }
+
+      if (days < 30) {
+        final weeks = (days / 7).truncate();
+        return _futurePlural(text: 'week', value: weeks);
+      }
+
+      final months = (days / 30).truncate();
+      return _futurePlural(text: 'month', value: months);
+    }
+  }
+
+  static String _pastPlural({String text, int value}) {
+    if (value == 1) {
+      return 'Last $text';
+    } else {
+      return '$value ${text}s ago';
+    }
+  }
+
+  static String _futurePlural({String text, int value}) {
+    if (value == 1) {
+      return 'Next $text';
+    } else {
+      return '$value ${text}s';
     }
   }
 }
