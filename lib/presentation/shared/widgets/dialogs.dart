9d45df0b6568cc8de01cdbff7d35574f076ed1e2
aleksanderwozniak <aleksanderwozniak96@gmail.com>
2018-12-12 15:03:18 +0100

    Add Menu to ListScreen, add PP dialog

--- lib/presentation/shared/widgets/dialogs.dart
+++ lib/presentation/shared/widgets/dialogs.dart
@@ -4,8 +4,14 @@ import 'package:flutter/material.dart';
 class RoundedAlertDialog extends StatelessWidget {
   final String title;
   final List<Widget> actions;
+  final Widget content;
 
-  const RoundedAlertDialog({Key key, this.title, this.actions}) : super(key: key);
+  const RoundedAlertDialog({
+    Key key,
+    this.title,
+    this.actions,
+    this.content,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -18,6 +24,7 @@ class RoundedAlertDialog extends StatelessWidget {
         borderRadius: BorderRadius.circular(12.0),
         side: BorderSide(width: 1.0, color: ColorfulApp.of(context).colors.bleak),
       ),
+      content: content,
       actions: actions,
     );
   }
